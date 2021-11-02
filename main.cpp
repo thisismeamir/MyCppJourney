369d27a027bec7d414c7e0624b7e2006503e0bdb
Amir Hosein <thisismeamir@outlook.com>
2021-11-02 13:54:26 +0330

    Numbers and math functions

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+
 
 using namespace std;
 
@@ -34,6 +36,25 @@ int main()
 
     cout << myname.find("mir",0) << endl;
     cout << myname.substr(1,3) << endl;
+
+
+    // Numbers
+    int a = 10;
+    int b = 33;
+    int c = a+b;
+    int d = a-b;
+    int e = a*b;
+    int f = a/b;
+    int g = a%b;
+    cout << a <<b <<c <<d<<e<<f<<g <<endl;
+
+    // using cmath
+    cout << pow(2,5) << endl;
+    cout << "Powered 2^5" <<endl;
+
+    cout << sqrt(32) << "Square root of 32." << endl;
+    cout << fmin(3,13) << "  " ;
+    cout << fmax(3,41);
 
     return 0;
 }
