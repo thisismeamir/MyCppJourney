663c7e4ecb9715afd5aba856f203b254aab76ff7
Amir Hosein <thisismeamir@outlook.com>
2021-11-02 12:53:21 +0330

    I did a program in C++!

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "John";
+    int characterAge;
+    characterAge = 30;
+
+    cout << "Hello world! " << characterName << endl;
+    cout << "Work with me :\)" << endl;
+
+    return 0;
+}
