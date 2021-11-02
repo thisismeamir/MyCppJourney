254e70a92a7d51fd9af27e1bfbc7ece7bc8e76a2
Amir Hosein <thisismeamir@outlook.com>
2021-11-02 15:29:18 +0330

    switch

--- switch.cpp
+++ switch.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+string getdow(int daynum){
+
+    string dayname;
+
+    switch(daynum){
+    case 0:
+        dayname = "sunday";
+        break;
+    case 1:
+        dayname = "monday";
+        break;
+    case 2:
+        dayname = "tuesday";
+        break;
+    case 3:
+        dayname = "wendsday";
+        break;
+    case 4:
+        dayname = "thursday";
+        break;
+    case 5:
+        dayname = "friday";
+        break;
+    case 6:
+        dayname = "saturday";
+        break;
+    default:
+        dayname = "invalid";
+        break;
+
+
+    }
+
+
+    return dayname;
+}
+
+int switches()
+{
+    // Switch
+
+    cout << getdow(12);
+    return 0;
+}
 
